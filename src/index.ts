42e55e7d472fcff03f7909ede86a024f86304a98
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-01-20 10:21:59 -0500

    Use evaluateExpression in evaluateFormulae

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import { evaluateExpression } from "./calculator/calculator";
+
 const evaluateFormulae = (formulae: Record<string, string>): Record<string, number> => {
   const result = {} as Record<string, number>;
 
   Object.keys(formulae).forEach((key) => {
-    result[key] = parseInt(formulae[key], 10);
+    result[key] = evaluateExpression(formulae[key]);
   });
 
   return result;
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,3 +3,24 @@ import evaluateFormulae from '../src/index';
 it('returns numbers unchanged', () => {
   expect(evaluateFormulae({ foo: '0', bar: '1' })).toEqual({ foo: 0, bar: 1 });
 });
+
+it('handles multiplication', () => {
+  expect(evaluateFormulae({ foo: '2*3*3', bar: '1' })).toEqual({ foo: 18, bar: 1 });
+});
+
+it('handles addition', () => {
+  expect(evaluateFormulae({ foo: '1+1+1', bar: '1' })).toEqual({ foo: 3, bar: 1 });
+});
+
+it('handles subtraction', () => {
+  expect(evaluateFormulae({ foo: '1-2-1', bar: '1' })).toEqual({ foo: -2, bar: 1 });
+});
+
+it('handles division', () => {
+  expect(evaluateFormulae({ foo: '4/2/2', bar: '1' })).toEqual({ foo: 1, bar: 1 });
+});
+
+it('handles multiplication and addition in the correct order', () => {
+  expect(evaluateFormulae({ foo: '1+2*2', bar: '1' })).toEqual({ foo: 5, bar: 1 });
+  expect(evaluateFormulae({ foo: '2*2+1', bar: '1' })).toEqual({ foo: 5, bar: 1 });
+});
